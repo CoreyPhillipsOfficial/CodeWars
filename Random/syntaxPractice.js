cd74ccffed2aeeed2a0edc9be374245cbec67cc2
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-04-09 20:58:46 -0400

    class Car

--- Random/syntaxPractice.js
+++ Random/syntaxPractice.js
@@ -1,3 +1,4 @@
+// Define a class called Person
 class Person {
     constructor(name, age) {
         this.name = name;
@@ -12,3 +13,23 @@ class Person {
 const person1 = new Person('Alice', 30);
 
 person1.displayInfo();
+
+
+
+// Define a class called Car
+class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    displayInfo() {
+        console.log(`Make: ${this.make}, Model: ${this.model}`);
+    }
+}
+
+// Creat an object of the Car class
+const car1 = new Car('Porsche', '911');
+
+// Call the displayInfo method to display car information
+car1.displayInfo();
