ece5b4359e88c824c2a505c32c11b6f536f271ef
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-05-09 18:37:17 -0400

    much shorter solution

--- 6th_Kyu/sumOfDigits.js
+++ 6th_Kyu/sumOfDigits.js
@@ -10,21 +10,29 @@ Examples
 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
 */
 
+
+
+// function digitalRoot(n) {
+//     // initializing sum as a variable
+//     let sum = 0;
+
+//     // loop to add the digits of the first number
+//     while (n) {
+//         sum += n % 10; // add last digit digit of n to sum
+//         n = Math.floor(n / 10); //remove last digit of n
+//     }
+
+//     // check if sum is a single digit
+//     if (sum < 10) {
+//         return sum; // if so, return the sum
+//     }
+//     return digitalRoot(sum); // if not, recursively call digitalRoot with the sum
+// }
+
+
+
 function digitalRoot(n) {
-    // initializing sum as a variable
-    let sum = 0;
-
-    // loop to add the digits of the first number
-    while (n) {
-        sum += n % 10; // add last digit digit of n to sum
-        n = Math.floor(n / 10); //remove last digit of n
-    }
-
-    // check if sum is a single digit
-    if (sum < 10) {
-        return sum; // if so, return the sum
-    }
-    return digitalRoot(sum); // if not, recursively call digitalRoot with the sum
+    return (n - 1) % 9 + 1;
 }
 
 // Tests
@@ -33,7 +41,12 @@ value2 = 942;
 value3 = 132189;
 value4 = 493193;
 
+
 console.log(digitalRoot(value1));
 console.log(digitalRoot(value2));
 console.log(digitalRoot(value3));
 console.log(digitalRoot(value4));
+
+
+
+
