54b28db1f0654b785ce2cec9e134f0bfb3bbf024
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-06-04 13:17:20 -0400

    completed divideAndConquer

--- 7th_Kyu/divideAndConquer.js
+++ 7th_Kyu/divideAndConquer.js
@@ -3,9 +3,17 @@
 // Return as a number.
 
 function divCon(x) {
-    let stringInt = 0
-    for (let i = 0; i < x.length; i++) {
-        const element = x[i];
-
+    let result = 0
+    for (i = 0; i < x.length; i++) {
+        if (typeof x[i] == 'number') {
+            result += x[i]
+        } else {
+            result -= Number(x[i])
+        }
     }
+    return result
 }
+
+console.log(divCon([9, 3, '7', '3']))
+console.log(divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
+console.log(divCon(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']))
