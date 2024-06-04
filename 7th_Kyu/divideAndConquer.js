4eda18394d4f802a8d1748fc2d396843fc69e8ab
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-06-04 13:11:16 -0400

    starting divideAndConquer problem

---
+++ 7th_Kyu/divideAndConquer.js
@@ -0,0 +1,11 @@
+// Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+
+// Return as a number.
+
+function divCon(x) {
+    let stringInt = 0
+    for (let i = 0; i < x.length; i++) {
+        const element = x[i];
+
+    }
+}
