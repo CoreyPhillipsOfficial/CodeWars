66f9447d51ead4ad1e2afb5a63203c7f07923653
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-06-13 09:07:21 -0400

    started give me a diamond

--- 5th_Kyu/peteTheBaker.js
+++ 5th_Kyu/peteTheBaker.js
@@ -4,6 +4,7 @@ Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunatel
 Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
 */
 
+
 function cakes(recipe, available) {
     return Object.keys(recipe).reduce(function (val, ingredient) {
         return Math.min(Math.floor(available[ingredient] / recipe[ingredient] || 0), val)
